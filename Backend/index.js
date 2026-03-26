a6ef32709d735715a0553a26505b79addb39280a
Sanju <sanjusawariya88@gmail.com>
2026-03-26 18:31:46 +0530

    Update CORS origin to production URL

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-z7wu.onrender.com",
     credentials: true
 }))
 app.use(cookieParser())
